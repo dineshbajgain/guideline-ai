db4c8a1084120cf6b89f9e1346f9bdc37333abeb
dinesh bajgain <dinesh.bajgain@monotype.com>
2024-06-10 23:59:57 +0545

    removed openai and added hugging face

--- gen_ai.py
+++ gen_ai.py
@@ -1,12 +1,28 @@
-import openai
+import os
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, pipeline
 
-openai.api_key = ''
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+
+# Set pad_token_id
+if tokenizer.pad_token is None:
+    tokenizer.pad_token = tokenizer.eos_token
+
+generator = pipeline('text-generation', model=model, tokenizer=tokenizer)
 
 def generate_learning_resources(dependency):
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Generate learning resources for {dependency}.",
-        max_tokens=100
-    )
-    resources = response.choices[0].text.strip().split('\n')
-    return [resource for resource in resources if resource]
+    resources = generator(f"Generate learning resources for {dependency}.", max_length=500)[0]['generated_text']
+    resources = resources.split('\n')
+    resources =  [resource for resource in resources if resource]
+     # Define subfolder path
+    subfolder_path = os.path.join('output', dependency)
+     # Ensure subfolder exists
+    if not os.path.exists(subfolder_path):
+        os.makedirs(subfolder_path)
+    
+    # Save to file
+    with open(os.path.join(subfolder_path, f'documnet.txt'), 'w') as f:
+        for resource in resources:
+            f.write(resource + '\n')
+            
+    return resources
